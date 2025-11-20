d742f06ccb0ac8d62d54d2fb81ee9e9043ce0740
buttiepedro <buttiepedro.bit@gmail.com>
2025-11-20 20:55:10 -0300

    preparar para easypanel

--- app/app.py
+++ app/app.py
@@ -26,4 +26,4 @@ def register():
     return redirect("/?success=1")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=5000)
--- app/db.py
+++ app/db.py
@@ -2,6 +2,10 @@ import os
 from pymongo import MongoClient
 
 mongo_uri = os.getenv("MONGO_URI")
+
+if not mongo_uri:
+    raise ValueError("ERROR: La variable de entorno MONGO_URI no está definida.")
+
 client = MongoClient(mongo_uri)
 
 db = client["usuarios_db"]
